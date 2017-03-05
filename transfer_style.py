01191a610e24ad9f2a616680d3c57e72c39c02d9
naresh1318 <rnaresh.n@gmail.com>
2017-03-05 22:55:20 +0530

    removed progressbar and added script to convert images to video

--- transfer_style.py
+++ transfer_style.py
@@ -1,23 +1,18 @@
 from __future__ import print_function
+import time
+import os
 import numpy as np
-from PIL import Image
 import keras.backend as K
+from video_to_frame import get_frames
+from PIL import Image
 from argparse import ArgumentParser
 from keras.applications import vgg16
-import progressbar
 from scipy.optimize import fmin_l_bfgs_b
 from scipy.misc import imsave
 
 # Parameters
 height = 512
 width = 512
-
-# Progressbar setup
-bar = progressbar.ProgressBar(widgets=[
-    ' [', progressbar.Timer(), ']',
-    progressbar.Bar(),
-    '(', progressbar.ETA(), ')'
-])
 
 
 def create_args():
@@ -33,7 +28,10 @@ def create_args():
     parser.add_argument("--total_variation_weight", "-t_v_w",
                         help="Weight that reduces the noise in the image", default=1.0, type=float)
     parser.add_argument("--epochs", "-e",
-                        help="No of epochs to optimize", default=10, type=int)
+                        help="No of epochs to optimize", default=3, type=int)
+    parser.add_argument("--video", "-v",
+                        help="Indicate whether you want to perform style transfer on video or an image",
+                        action='store_true', dest="video")
     return parser
 
 
@@ -123,6 +121,7 @@ class Evaluator(object):
         self.grad_values = None
         return grad_values
 
+
 parser = create_args()
 options = parser.parse_args()
 
@@ -131,73 +130,91 @@ content_weight = options.content_weight  # 0.025
 style_weight = options.style_weight  # 5.0
 total_variation_weight = options.total_variation_weight  # 1.0
 
-# Get the content image
-content_image_path = options.content_path
-content_image = Image.open(content_image_path)
-content_array = preprocess(content_image)
-
 # Get the style image
 style_image_path = options.style_path
 style_image = Image.open(style_image_path)
 style_array = preprocess(style_image)
 
-# Tensorflow variables and placeholders for the graph
-content_image = K.variable(content_array)
-style_image = K.variable(style_array)
-combined_image = K.placeholder((1, height, width, 3))
-
-# Concatenate into a single tensor
-input_tensor = K.concatenate([content_image, style_image, combined_image], axis=0)
-
-# Load the weights for vgg16
-model = vgg16.VGG16(input_tensor=input_tensor, include_top=False, weights='imagenet')
-
-# Get all the layers in vgg16
-layers = dict([(layer.name, layer.output) for layer in model.layers])
-
-loss = K.variable(0.)  # Default dtype = float32
-
-# Calculate the content loss
-layer_features_content = layers['block2_conv2']
-content_image_features = layer_features_content[0, :, :, :]
-combined_image_features = layer_features_content[2, :, :, :]
-loss += content_weight * content_loss(content_image_features, combined_image_features)
-
-# Calculate the style loss
-layer_features = ['block1_conv2', 'block2_conv2',
-                  'block3_conv3', 'block4_conv3',
-                  'block5_conv3']
-
-for layer in layer_features:
-    layer_features_style = layers[layer]
-    style_image_features = layer_features_style[1, :, :, :]
-    combined_image_features = layer_features_style[2, :, :, :]
-    loss += (style_weight / len(layer_features)) * style_loss(style_image_features, combined_image_features)
-
-# Compute the total variation loss
-loss += total_variation_weight * total_variation_loss(combined_image)
-
-# Find the gradients
-grads = K.gradients(loss, combined_image)
-outputs = [loss]
-
-if type(grads) in {list, tuple}:
-    outputs += grads
+# Check whether to style video or an image
+if options.video:
+    frames = get_frames(options.content_path)
 else:
-    outputs.append(grads)
-
-f_outputs = K.function([combined_image], outputs)
-
-evaluator = Evaluator()
+    frames = [options.content_path]
 
 # Training
-combined_img = np.random.uniform(0, 255, (1, height, width, 3)) - 128.
+x = np.random.uniform(0, 255, (1, height, width, 3)) - 128.
 
-epochs = options.epochs
+# Get the content image
+for j, frame in enumerate(frames):
+    content_image = Image.open(frame)
+    content_array = preprocess(content_image)
 
-for i in bar(range(epochs)):
-    combined_img, min_val, info = fmin_l_bfgs_b(evaluator.loss, combined_img.flatten(),
-                                                fprime=evaluator.grads, maxfun=20)
-    # Save image after each epoch
-    save_com = remove_preprocess(combined_img)
-    imsave('combined_image_{}.jpg'.format(i), Image.fromarray(save_com))
+    # Tensorflow variables and placeholders for the graph
+    content_image = K.variable(content_array)
+    style_image = K.variable(style_array)
+    combined_image = K.placeholder((1, height, width, 3))
+
+    # Concatenate into a single tensor
+    input_tensor = K.concatenate([content_image, style_image, combined_image], axis=0)
+
+    # Load the weights for vgg16
+    model = vgg16.VGG16(input_tensor=input_tensor, include_top=False, weights='imagenet')
+
+    # Get all the layers in vgg16
+    layers = dict([(layer.name, layer.output) for layer in model.layers])
+
+    loss = K.variable(0.)  # Default dtype = float32
+
+    # Calculate the content loss
+    layer_features_content = layers['block2_conv2']
+    content_image_features = layer_features_content[0, :, :, :]
+    combined_image_features = layer_features_content[2, :, :, :]
+    loss += content_weight * content_loss(content_image_features, combined_image_features)
+
+    # Calculate the style loss
+    layer_features = ['block1_conv2', 'block2_conv2',
+                      'block3_conv3', 'block4_conv3',
+                      'block5_conv3']
+
+    for layer in layer_features:
+        layer_features_style = layers[layer]
+        style_image_features = layer_features_style[1, :, :, :]
+        combined_image_features = layer_features_style[2, :, :, :]
+        loss += (style_weight / len(layer_features)) * style_loss(style_image_features, combined_image_features)
+
+    # Compute the total variation loss
+    loss += total_variation_weight * total_variation_loss(combined_image)
+
+    # Find the gradients
+    grads = K.gradients(loss, combined_image)
+    outputs = [loss]
+
+    if type(grads) in {list, tuple}:
+        outputs += grads
+    else:
+        outputs.append(grads)
+
+    f_outputs = K.function([combined_image], outputs)
+
+    evaluator = Evaluator()
+
+    epochs = options.epochs
+
+    for i in range(epochs):
+        print('Start of iteration', i)
+        start_time = time.time()
+        x, min_val, info = fmin_l_bfgs_b(evaluator.loss, x.flatten(),
+                                         fprime=evaluator.grads, maxfun=20)
+        print('Current loss value:', min_val)
+        end_time = time.time()
+        print('Iteration %d completed in %ds' % (i, end_time - start_time))
+    x = x.reshape((height, width, 3))
+    x = x[:, :, ::-1]  # Back to RGB
+    x[:, :, 0] += 103.939
+    x[:, :, 1] += 116.779
+    x[:, :, 2] += 123.68
+    x = np.clip(x, 0, 255).astype('uint8')
+    imsave('combined_dh_{}.jpg'.format(j), Image.fromarray(x))
+
+# execute this linux command to convert the video images to a video
+os.system('ffmpeg -f image2 -r 1/5 -i {}%d.jpg -vcodec mpeg4 -y movie.mp4'.format('combined_dh_'))
